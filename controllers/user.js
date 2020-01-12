7fad696e023967fc323bbd474990f5ba686b4917
Daniel Seong <dseong0224@gmail.com>
2020-01-11 23:12:02 -0800

    duplicate sign up input troubleshooting sign up should be prevented when user inputs identical name or email

--- controllers/user.js
+++ controllers/user.js
@@ -1,7 +1,10 @@
 const User = require("../models/user");
 
 exports.signup = (req, res) => {
-  console.log("req.body: ", req.body);
+  if( req.body ) {
+    console.log(`req.body1: ${req}`);
+  }
+  console.log("req.body2: ", req.body);
   const user = new User(req.body);
   user.save((err, user) => {
     if (err) {
